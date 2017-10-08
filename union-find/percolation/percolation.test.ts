eceed94d4a50dafa7094071b12282193885c827c
Xavier Francis <thisisxvr@gmail.com>
2017-10-08 18:13:24 +1300

    Implement percolation methods
    Need to create virtual sites and implement rest of the methods.

--- union-find/percolation/percolation.test.ts
+++ union-find/percolation/percolation.test.ts
@@ -26,56 +26,56 @@ describe('A percolation system', () => {
     })
 
     it('with a Quick Union algorithm that has no connections', () => {
-      expect(system.connectivity.id).toBe([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
+      expect(system.sites.id).toBe([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
     })
 
     it('with a Quick Union algorithm that has no trees', () => {
-      expect(system.connectivity.sz).toBe([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+      expect(system.sites.sz).toBe([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
     })
   })
 
   describe('can tell site vacancy for...', () => {
     it('a 5 by 5 grid that is fully blocked', () => {
       const gridDimension = system.grid.length
-      for (let i = 0; i < gridDimension; i++) {
-        for (let j = 0; j < gridDimension; j++) {
+      for (let i = 1; i <= gridDimension; i++) {
+        for (let j = 1; j <= gridDimension; j++) {
           expect(system.isOpen(i, j)).toBe(false)
         }
       }
     })
 
     it('a 5 by 5 grid that is partially blocked', () => {
-      system.open(0, 0)
-      system.open(0, 1)
-      system.open(0, 3)
-      system.open(1, 3)
-      system.open(2, 1)
-      system.open(2, 3)
+      system.open(1, 1)
+      system.open(1, 2)
+      system.open(1, 4)
       system.open(2, 4)
+      system.open(3, 2)
+      system.open(3, 4)
+      system.open(3, 5)
 
-      expect(system.isOpen(0, 0)).toBe(true)
-      expect(system.isOpen(0, 1)).toBe(true)
-      expect(system.isOpen(0, 2)).toBe(false)
-      expect(system.isOpen(0, 3)).toBe(true)
-      expect(system.isOpen(0, 4)).toBe(false)
       expect(system.isOpen(1, 1)).toBe(true)
-      expect(system.isOpen(2, 1)).toBe(true)
-      expect(system.isOpen(2, 3)).toBe(true)
+      expect(system.isOpen(1, 2)).toBe(true)
+      expect(system.isOpen(1, 4)).toBe(true)
       expect(system.isOpen(2, 4)).toBe(true)
+      expect(system.isOpen(3, 2)).toBe(true)
+      expect(system.isOpen(3, 4)).toBe(true)
+      expect(system.isOpen(3, 5)).toBe(true)
+      expect(system.isOpen(0, 2)).toBe(false)
+      expect(system.isOpen(0, 4)).toBe(false)
     })
 
     it('a 5 by 5 grid that is fully open', () => {
       const gridDimension = system.grid.length
       // Open all sites
-      for (let i = 0; i < gridDimension; i++) {
-        for (let j = 0; j < gridDimension; j++) {
+      for (let i = 1; i <= gridDimension; i++) {
+        for (let j = 1; j <= gridDimension; j++) {
           system.open(i, j)
         }
       }
 
       // Verify sites are open
-      for (let i = 0; i < gridDimension; i++) {
-        for (let j = 0; j < gridDimension; j++) {
+      for (let i = 1; i <= gridDimension; i++) {
+        for (let j = 1; j <= gridDimension; j++) {
           expect(system.isOpen(i, j)).toBe(true)
         }
       }
@@ -86,15 +86,15 @@ describe('A percolation system', () => {
     let system: Percolation
     beforeEach(() => {
       system = new Percolation(5)
-      system.open(0, 0)
-      system.open(0, 1)
-      system.open(0, 3)
+      system.open(1, 1)
+      system.open(1, 2)
+      system.open(1, 4)
     })
 
     it('for a small grid', () => {
-      expect(system.isFull(0, 0)).toEqual(true)
-      expect(system.isFull(0, 1)).toEqual(true)
-      expect(system.isFull(0, 3)).toEqual(true)
+      expect(system.isFull(1, 1)).toEqual(true)
+      expect(system.isFull(1, 2)).toEqual(true)
+      expect(system.isFull(1, 4)).toEqual(true)
     })
   })
 
@@ -102,13 +102,13 @@ describe('A percolation system', () => {
     let system: Percolation
     beforeEach(() => {
       system = new Percolation(5)
-      system.open(0, 0)
-      system.open(0, 1)
-      system.open(0, 3)
-      system.open(1, 3)
-      system.open(2, 1)
-      system.open(2, 3)
+      system.open(1, 1)
+      system.open(1, 2)
+      system.open(1, 4)
       system.open(2, 4)
+      system.open(3, 2)
+      system.open(3, 4)
+      system.open(3, 5)
     })
 
     it('for a small grid', () => {
@@ -118,8 +118,8 @@ describe('A percolation system', () => {
     it('for a fully open grid', () => {
       const gridDimension = system.grid.length
       // Open all sites
-      for (let i = 0; i < gridDimension; i++) {
-        for (let j = 0; j < gridDimension; j++) {
+      for (let i = 1; i <= gridDimension; i++) {
+        for (let j = 1; j <= gridDimension; j++) {
           system.open(i, j)
         }
       }
@@ -128,29 +128,29 @@ describe('A percolation system', () => {
     })
   })
 
-  describe('check for percolation', () => {
+  describe('correctly asserts percolation...', () => {
     beforeEach(() => {
-      system.open(0, 0)
-      system.open(0, 1)
-      system.open(0, 3)
-      system.open(1, 3)
-      system.open(2, 1)
-      system.open(2, 3)
+      system.open(1, 1)
+      system.open(1, 2)
+      system.open(1, 4)
       system.open(2, 4)
-      system.open(3, 0)
       system.open(3, 2)
-      system.open(4, 0)
+      system.open(3, 4)
+      system.open(3, 5)
       system.open(4, 1)
       system.open(4, 3)
-      system.open(4, 4)
+      system.open(5, 1)
+      system.open(5, 2)
+      system.open(5, 4)
+      system.open(5, 5)
     })
 
     it('in a system that does not percolate', () => {
       expect(system.percolates).toBe(false)
     })
 
-    it('... and when it does percolate', () => {
-      system.open(3, 3)
+    it('and in one that does', () => {
+      system.open(4, 4)
       expect(system.percolates).toBe(true)
     })
   })
--- union-find/percolation/percolation.ts
+++ union-find/percolation/percolation.ts
@@ -1,23 +1,69 @@
 import UnionFind from '../weighted-quick-union-path-compressed'
 
-export default class Percolation {
+interface ISite {
+  index: number
+  isOpen: boolean
+}
 
-  private _connectivity: UnionFind
-  private _grid: number[][]
+export default class Percolation {
+  private _sites: UnionFind
+  private _grid: ISite[][]
   get grid() { return this._grid }
-  get connectivity() { return this._connectivity }
+  get sites() { return this._sites }
 
   // Creates an n-by-n grid, with all sites blocked
-  constructor(n: number) {}
+  constructor(n: number) {
+    if (n <= 0) { throw new RangeError('A percolation system must be sized to a positive integer.') }
+
+    let name = 0
+    this._grid = []
+    for (let i = 0; i < n; i++) {
+      while (name < (n ** 2)) {
+        const row = []
+        for (let j = 0; j < n; j++) {
+          row[j] = { index: name, isOpen: false }
+          name++
+        }
+        this._grid[i] = row
+      }
+    }
+
+    this._sites = new UnionFind(n ** 2)
+  }
 
   // Open site if not already open
-  open(row: number, column: number): void {}
+  open(row: number, column: number): void {
+    if (this.indexOutsideRange(row, column)) {
+      throw new RangeError(`Row and column indicies must be between 1 and ${this._grid.length}`)
+    }
+
+    if (this.isOpen(row, column)) { return }
+
+    this.meetTheNeighbours(row, column)
+    const site = this._grid[row--][column--]
+    site.isOpen = true
+  }
 
   // Is site open?
-  isOpen(row: number, column: number): boolean {}
+  isOpen(row: number, column: number): boolean {
+    if (this.indexOutsideRange(row, column)) {
+      throw new RangeError(`Row and column indicies must be between 1 and ${this._grid.length}`)
+    }
+
+    const site = this._grid[row--][column--]
+    return site.isOpen
+  }
 
   // Is site full?
-  isFull(row: number, column: number): boolean {}
+  isFull(row: number, column: number): boolean {
+    if (this.indexOutsideRange(row, column)) {
+      throw new RangeError(`Row and column indicies must be between 1 and ${this._grid.length}`)
+    }
+
+    const site = this._grid[row--][column--]
+
+    return false
+  }
 
   // Returns the number of open sites
   numberOfOpenSites(): number {}
@@ -25,4 +71,36 @@ export default class Percolation {
   // Does the system percolate?
   percolates(): boolean {}
 
+  private meetTheNeighbours(row: number, column: number): void {
+    const siteRow = row - 1
+    const siteColumn = column - 1
+    const site = this._grid[siteRow][siteColumn]
+
+    let topSite: ISite
+    let rightSite: ISite
+    let bottomSite: ISite
+    let leftSite: ISite
+
+    // Get the neighbours and connect them
+    if (row > 1) {
+      topSite = this._grid[siteRow - 1][siteColumn]
+      if (topSite.isOpen) { this._sites.union(site.index, topSite.index) }
+    }
+    if (row < this._grid.length) {
+      bottomSite = this._grid[siteRow + 1][siteColumn]
+      if (bottomSite.isOpen) { this._sites.union(site.index, bottomSite.index) }
+    }
+    if (column > 1) {
+      leftSite = this._grid[siteRow][siteColumn - 1]
+      if (leftSite.isOpen) { this._sites.union(site.index, leftSite.index) }
+    }
+    if (column < this._grid.length) {
+      rightSite = this._grid[siteRow][siteColumn + 1]
+      if (rightSite.isOpen) { this._sites.union(site.index, rightSite.index) }
+    }
+  }
+
+  private indexOutsideRange(row: number, column: number): boolean {
+    return row < 1 || row > this._grid.length || column < 1 || column > this._grid.length
+  }
 }
