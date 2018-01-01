90e99b7a6f96317e032f775341763477325fd226
Xavier Francis <thisisxvr@gmail.com>
2018-01-01 18:57:18 +1300

    Deque complete with tests passing

--- stacks-and-queues/deques-and-randomized-queues/deque.test.ts
+++ stacks-and-queues/deques-and-randomized-queues/deque.test.ts
@@ -1,5 +1,108 @@
+import Deque from './deque'
+
 describe('A Deque should', () => {
+  let deque: Deque<string>
 
-  //
+  it('work with any data type', () => {
+    expect(new Deque<number>()).toBeInstanceOf(Deque)
+    expect(new Deque<boolean>()).toBeInstanceOf(Deque)
+    expect(new Deque<object>()).toBeInstanceOf(Deque)
+  })
 
+  it('verify that it is empty', () => {
+    deque = new Deque<string>()
+    expect(deque.isEmpty()).toBeTruthy()
+  })
+
+  it('add items to the front', () => {
+    deque.addFirst('to')
+    deque.addFirst('be')
+    deque.addFirst('or')
+    deque.addFirst('not')
+    deque.addFirst('to')
+    deque.addFirst('be')
+  })
+
+  it('add items to the back', () => {
+    deque.addLast('that')
+    deque.addLast('is')
+    deque.addLast('the')
+    deque.addLast('question')
+  })
+
+  it('remove and return the items from the front', () => {
+    expect(deque.removeFirst()).toBe('be')
+    expect(deque.removeFirst()).toBe('to')
+    expect(deque.removeFirst()).toBe('not')
+  })
+
+  it('remove and return the item from the end', () => {
+    expect(deque.removeLast()).toBe('question')
+    expect(deque.removeLast()).toBe('the')
+    expect(deque.removeLast()).toBe('is')
+  })
+
+  it('return the number of items on the deque', () => {
+    expect(deque.size()).toBe(4)
+  })
+
+  it('empty completely', () => {
+    expect(deque.removeFirst()).toBe('or')
+    expect(deque.removeLast()).toBe('that')
+    expect(deque.removeFirst()).toBe('be')
+    expect(deque.removeLast()).toBe('to')
+    expect(deque.size()).toBe(0)
+  })
+
+  it('throws when passed a null argument', () => {
+    const addFirstError = () => { deque.addFirst(null!) }
+    expect(addFirstError).toThrow(TypeError)
+    const addLastError = () => { deque.addLast(null!) }
+    expect(addLastError).toThrow(TypeError)
+  })
+
+  it('throws when a remove is attempted on an empty deque', () => {
+    const removeFirstError = () => { deque.removeFirst() }
+    expect(removeFirstError).toThrow('Deque is empty.')
+    const removeLastError = () => { deque.removeLast() }
+    expect(removeLastError).toThrow('Deque is empty.')
+  })
+
+  it('continue to operate as expected', () => {
+    deque.addFirst('the')
+    deque.addLast('was')
+    deque.addFirst('question')
+    deque.addLast('what')
+    deque.addFirst('again')
+    deque.addFirst('?')
+
+    expect(deque.size()).toBe(6)
+
+    expect(deque.removeLast()).toBe('what')
+    expect(deque.removeLast()).toBe('was')
+    expect(deque.removeLast()).toBe('the')
+    expect(deque.removeLast()).toBe('question')
+    expect(deque.removeFirst()).toBe('?')
+    expect(deque.removeLast()).toBe('again')
+
+    expect(deque.isEmpty()).toBeTruthy()
+  })
+
+  it('has an iterator that returns the next value', () => {
+    const dequeItems = ['what', 'was', 'the', 'question', 'again', '?']
+
+    deque.addLast('the')
+    deque.addLast('was')
+    deque.addFirst('question')
+    deque.addFirst('what')
+    deque.addFirst('again')
+    deque.addLast('?')
+
+    for (const item of deque) {
+      expect(dequeItems).toContain(item)
+    }
+
+    expect(deque.size()).toBe(0)
+    expect(deque.isEmpty()).toBeTruthy()
+  })
 })
--- stacks-and-queues/deques-and-randomized-queues/deque.ts
+++ stacks-and-queues/deques-and-randomized-queues/deque.ts
@@ -2,7 +2,7 @@ class Node<T> {
   public previous: Node<T> | null
   public value: T
   public next: Node<T> | null
-  constructor(val: T) { this.value = val }
+  constructor(val: T) { this.value = val, this.previous = null, this.next = null }
 }
 
 export default class Deque<T> {
@@ -16,34 +16,93 @@ export default class Deque<T> {
   }
 
   /** Is the deque empty? */
-  isEmpty(): boolean {
-    //
-  }
+  isEmpty(): boolean { return this.count === 0 }
 
   /** Returns the number of items on the deque.*/
-  size(): number {
-    //
-  }
+  size(): number { return this._count }
 
   /** Add an item to the front. */
   addFirst(item: T) {
-    //
+    if (item === null || undefined) { throw new TypeError() }
+
+    const newNode = new Node<T>(item)
+
+    if (this.isEmpty()) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      const oldHead = this.head
+      oldHead!.next = newNode
+      newNode.previous = oldHead
+      this.head = newNode
+    }
+
+    this._count++
   }
 
   /** Add an item to the end. */
   addLast(item: T) {
-    //
+    if (item === null || undefined) { throw new TypeError() }
+
+    const newNode = new Node<T>(item)
+
+    if (this.isEmpty()) {
+      this.tail = newNode
+      this.head = newNode
+    } else {
+      const oldTail = this.tail
+      oldTail!.previous = newNode
+      newNode.next = oldTail
+      this.tail = newNode
+    }
+
+    this._count++
   }
 
   /** Remove and return the item from the front. */
   removeFirst(): T {
-    //
+    if (this.isEmpty()) { throw new Error('Deque is empty.') }
+    const item = this.head!
+    const newHead = this.head!.previous
+
+    if (newHead) {
+      this.head = newHead
+      newHead.next = null
+    } else {
+      this.tail = null
+      this.head = null
+    }
+
+    this._count--
+    return item.value
   }
 
   /** Remove and return the item from the end. */
   removeLast(): T {
-    //
+    if (this.isEmpty()) { throw new Error('Deque is empty.') }
+    const item = this.tail!
+    const newTail = this.tail!.next
+
+    if (newTail) {
+      this.tail = newTail
+      newTail.previous = null
+    } else {
+      this.tail = null
+      this.head = null
+    }
+
+    this._count--
+    return item.value
   }
 
   // Iterator
+  [Symbol.iterator](): IterableIterator<T | null> { return this }
+
+  next(): IteratorResult<T | null> {
+    if (this.isEmpty()) {
+      return { done: true, value: null }
+    } else {
+      return { done: false, value: this.removeLast() }
+    }
+  }
 }
